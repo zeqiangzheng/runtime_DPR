bf69d47a2eaa7a8c1fd6aca1f8ca012b50abbda9
Zeqiang Zheng <55548698+zeqiangzheng@users.noreply.github.com>
2025-12-10 12:39:49 +0800

    Add files via upload

---
+++ pl_vecadd.cpp
@@ -0,0 +1,20 @@
+#include "ap_int.h"
+#include "ap_fixed.h"
+#include "hls_math.h"
+
+void pl_vecadd(float a[1024], float b[1024], float c[1024])
+{
+  #pragma HLS INTERFACE m_axi port=a offset=slave bundle=data0
+  #pragma HLS INTERFACE s_axilite register port=a bundle=ctrl
+  #pragma HLS INTERFACE m_axi port=b offset=slave bundle=data1
+  #pragma HLS INTERFACE s_axilite register port=b bundle=ctrl
+  #pragma HLS INTERFACE m_axi port=c offset=slave bundle=data2
+  #pragma HLS INTERFACE s_axilite register port=c bundle=ctrl
+  #pragma HLS INTERFACE s_axilite register port=return bundle=ctrl
+  for (int i = 0; i < 1024; i += 1)
+  {
+    #pragma HLS pipeline
+    c[i] = a[i] + b[i];
+  }
+
+}
